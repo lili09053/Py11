3e50688fcb3805cc2b161507fc906521dd183dbe
lili09053 <91773645+lili09053@users.noreply.github.com>
2021-11-02 13:46:00 +0500

    Add files via upload
    write function for pushButton

---
+++ focus.py
@@ -0,0 +1,41 @@
+import sys
+import pickle
+from PyQt5.QtWidgets import QApplication, QMainWindow
+from untitled import Ui_MainWindow
+
+
+class Focus(QMainWindow, Ui_MainWindow):
+
+    def __init__(self):
+        super().__init__()
+        self.setupUi(self)
+        self.pushButton_arrow.clicked.connect(self.button_clicked)
+
+    def button_clicked(self):
+        if self.pushButton_arrow.text() == "->":
+            self.pushButton_arrow.setText("<-")
+        else:
+            self.pushButton_arrow.setText("->")
+
+sys._excepthook = sys.excepthook
+
+
+def exception_hook(exctype, value, traceback):
+    sys._excepthook(exctype, value, traceback)
+    sys.exit(1)
+
+
+sys.excepthook = exception_hook
+
+
+if __name__ == '__main__':
+    sys.excepthook = exception_hook
+    app = QApplication(sys.argv)
+    wnd = Focus()
+    wnd.show()
+    sys.exit(app.exec())
+
+
+
+
+
