0b6f1f947b31da01c1fae2096ab9e44eced1ea2a
lili09053 <91773645+lili09053@users.noreply.github.com>
2021-11-03 14:55:35 +0500

    Update hide_and_seek.py
    Активизировала 2-4 кнопки

--- hw2/hide_and_seek.py
+++ hw2/hide_and_seek.py
@@ -8,12 +8,34 @@ class Hide_and_Seek(QMainWindow, Ui_MainWindow):
         super().__init__()
         self.setupUi(self)
         self.checkBox_1.stateChanged.connect(self.check1_clicked)
+        self.checkBox_2.stateChanged.connect(self.check2_clicked)
+        self.checkBox_3.stateChanged.connect(self.check3_clicked)
+        self.checkBox_4.stateChanged.connect(self.check4_clicked)
 
     def check1_clicked(self):
         if self.checkBox_1.isChecked():
             self.textBrowser_1.hide()
         else:
             self.textBrowser_1.show()
+    
+    def check2_clicked(self):
+        if self.checkBox_2.isChecked():
+            self.textBrowser_2.hide()
+        else:
+            self.textBrowser_2.show()
+
+    def check3_clicked(self):
+        if self.checkBox_3.isChecked():
+            self.textBrowser_3.hide()
+        else:
+            self.textBrowser_3.show()
+
+    def check4_clicked(self):
+        if self.checkBox_4.isChecked():
+            self.textBrowser_4.hide()
+        else:
+            self.textBrowser_4.show()
+
 
 sys._excepthook = sys.excepthook
 
